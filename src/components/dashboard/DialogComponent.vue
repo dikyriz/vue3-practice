cc439497604601c415ee4b2694643f1809d711ac
dikyriz <rizaldi.cool4@gmail.com>
2024-06-07 15:03:21 +0700

    part-31

---
+++ src/components/dashboard/DialogComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-dialog width="auto">
+    <v-card>
+      <v-card-title>
+        <slot name="title"></slot>
+      </v-card-title>
+      <v-card-text>
+        <slot name="content"></slot>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,9 +1,90 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue'
+import DialogComponent from '../components/dashboard/DialogComponent.vue'
+
+const dialog = ref(false)
+const form = ref(false)
+
+const categories = [
+  { id: 1, name: 'Politik' },
+  { id: 2, name: 'Kuliner' },
+  { id: 3, name: 'IT' },
+  { id: 4, name: 'Sejarah' }
+]
+
+const nameRules = [
+  (value) => {
+    if (value) return true
+    return 'Isi dahulu'
+  }
+]
+
+const descriptionRules = [
+  (value) => {
+    if (value) return true
+    return 'Isi dahulu'
+  }
+]
+
+const category = reactive({
+  name: '',
+  description: ''
+})
+
+const submitData = () => {
+  if (!form.value) return
+  alert('test')
+}
+</script>
 <template>
   <div>Category</div>
-  <v-list>
-    <v-list-item :to="{ name: 'Category-name', params: { name: '1' } }">1</v-list-item>
-    <v-list-item :to="{ name: 'Category-name', params: { name: '2' } }">2</v-list-item>
-    <v-list-item :to="{ name: 'Category-name', params: { name: '3' } }">3</v-list-item>
-  </v-list>
+  <div class="d-flex justify-end">
+    <v-btn color="primary" icon="mdi-plus" size="large" @click="dialog = true" />
+  </div>
+
+  <DialogComponent v-model="dialog">
+    <template #title?> Add Category </template>
+    <template #content>
+      <v-form v-model="form" @submit.prevent="submitData">
+        <v-text-field
+          class="mb-1"
+          :rules="nameRules"
+          v-model="category.name"
+          label="Name"
+        ></v-text-field>
+        <v-textarea
+          class="mb-1"
+          :rules="descriptionRules"
+          v-model="category.description"
+          label="Description"
+        ></v-textarea>
+        <v-btn :disabled="!form" block color="success" size="large" type="submit" variant="elevated"
+          >Add</v-btn
+        >
+      </v-form>
+    </template>
+  </DialogComponent>
+
+  <!-- Table -->
+
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">No</th>
+        <th class="text-left">Name</th>
+        <th class="text-center">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in categories" :key="item.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ item.name }}</td>
+        <td class="text-center">
+          <v-btn size="x-small" color="info" icon="mdi-information" />
+          <v-btn size="x-small" color="primary" icon="mdi-pencil" class="mx-3" />
+          <v-btn size="x-small" color="error" icon="mdi-trash-can" />
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
